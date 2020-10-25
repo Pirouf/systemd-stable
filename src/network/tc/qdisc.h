35942c8c9c6ca208abf0c1214dece09a1dda76e7
florent pirou <florent.pirou@intel.com>
2020-10-25 08:03:07 +0100

    network: tc: introduce Time Aware Priority Shaper (TAPRIO)
    https://man7.org/linux/man-pages/man8/tc-taprio.8.html
    
    Signed-off-by: florent pirou <florent.pirou@intel.com>

--- src/network/tc/qdisc.h
+++ src/network/tc/qdisc.h
@@ -31,6 +31,7 @@ typedef enum QDiscKind {
         QDISC_KIND_TBF,
         QDISC_KIND_TEQL,
         QDISC_KIND_ETF,
+        QDISC_KIND_TAPRIO,
         _QDISC_KIND_MAX,
         _QDISC_KIND_INVALID = -1,
 } QDiscKind;
@@ -107,3 +108,4 @@ CONFIG_PARSER_PROTOTYPE(config_parse_qdisc_handle);
 #include "tbf.h"
 #include "teql.h"
 #include "etf.h"
+#include "taprio.h"
